52022607d7aaf6f12d31d120b62c83c16aaeb9a8
taseenjeem <mdtaseenfuad@gmail.com>
2023-12-19 02:38:58 +0600

    Added a marquee

--- components/pages/LandingPage/HomePageStructure.jsx
+++ components/pages/LandingPage/HomePageStructure.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import NoticeSlide from "./NoticeSlide";
 
 const HomePageStructure = () => {
   return (
     <section>
       <Hero />
+      <NoticeSlide />
     </section>
   );
 };
---
+++ components/pages/LandingPage/NoticeSlide.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const NoticeSlide = () => {
+  return (
+    <Marquee className="bg-primary py-5">
+      <p className="text-lg text-white">
+        Unique Pathology - Your Health Hub! Expert diagnostics, compassionate
+        care. Visit us for precise assessments and personalized attention. Your
+        well-being matters to us!
+      </p>
+    </Marquee>
+  );
+};
+
+export default NoticeSlide;
