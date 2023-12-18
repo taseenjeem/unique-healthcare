17913509f7b0c7a438f6ab277a53bf9160bd7902
taseenjeem <mdtaseenfuad@gmail.com>
2023-12-18 23:56:36 +0600

    added TanStack Query

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import TanstackProvider from "@/providers/TanstackProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="wrapper">{children}</main>
+        <TanstackProvider>
+          <main className="wrapper">{children}</main>
+        </TanstackProvider>
       </body>
     </html>
   );
