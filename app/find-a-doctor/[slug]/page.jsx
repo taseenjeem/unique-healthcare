99a73a3d6b2ccc7d93a5074b7d5de8f374b2d5c2
taseenjeem <mdtaseenfuad@gmail.com>
2024-01-02 16:20:24 +0600

    added new page for doctor details

---
+++ app/find-a-doctor/[slug]/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export async function generateMetadata({ params }) {
+  const data = await fetch(
+    `http://localhost:8000/all-doctors-info-by-query?slug=${params.slug}`
+  ).then((res) => res.json());
+
+  return {
+    title: `${data[0]?.name} | Unique Pathology`,
+    description: data[0]?.education,
+  };
+}
+
+const page = () => {
+  return <div></div>;
+};
+
+export default page;
